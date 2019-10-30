598812d896af08a0c9b3c5f145611946e5bc6abc
nelsong27 <nelson.gomez@puppet.com>
2019-10-30 13:46:14 -0400

    update win reg key value 1

--- site-modules/example/manifests/windows_regkeys.pp
+++ site-modules/example/manifests/windows_regkeys.pp
@@ -8,7 +8,7 @@ class example::windows_regkeys {
 
   registry_value { 'HKEY_LOCAL_MACHINE\Software\Demonstration\value1':
     type => string,
-    data => 'this is a value from puppet demo',
+    data => 'this is a value from puppet demo1',
   }
 
   registry_value { 'HKEY_LOCAL_MACHINE\Software\Demonstration\value2':
