91ecd64ab64ec15abee41b892182b2d3ddc1e1ec
nelsong27 <nelson.gomez@puppet.com>
2020-03-25 15:43:41 -0400

    change port to 8080

--- site-modules/profile/manifests/app/sample_website/linux.pp
+++ site-modules/profile/manifests/app/sample_website/linux.pp
@@ -1,6 +1,6 @@
 class profile::app::sample_website::linux (
   String $doc_root = '/var/www/html',
-  Integer $webserver_port = 80,
+  Integer $webserver_port = 8080,
   String $website_source_dir = 'puppet:///modules/profile/app/sample_website'
 ) {
 
