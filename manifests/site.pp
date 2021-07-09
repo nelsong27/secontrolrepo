18535672e0ae963aff8566b0908b1a9224b45810
nelsong27 <nelson.gomez@puppet.com>
2021-07-09 09:13:38 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,14 +4,14 @@
 File { backup => false }
 
 # Applications managed by App Orchestrator are defined in the site block.
-site {
+#site {
 
-}
+#}
 
 node default {
   # Check if we've set the role for this node via trusted fact, pp_role.  If yes; include that role directly here.
-  if !empty( $trusted['extensions']['pp_role'] ) {
-    $role = $trusted['extensions']['pp_role']
-    include "role::${trusted['extensions']['pp_role']}"
-  }
+#  if !empty( $trusted['extensions']['pp_role'] ) {
+#    $role = $trusted['extensions']['pp_role']
+#   include "role::${trusted['extensions']['pp_role']}"
+#  }
 }
