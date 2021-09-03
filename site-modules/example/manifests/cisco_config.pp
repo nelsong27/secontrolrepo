b3b1599c73497c89c63e97cecaeb166f948fbbc1
nelsong27 <nelson.gomez@puppet.com>
2021-09-03 14:41:04 -0400

    Update cisco_config.pp

--- site-modules/example/manifests/cisco_config.pp
+++ site-modules/example/manifests/cisco_config.pp
@@ -1,10 +1,10 @@
 class example::cisco_config {
 
-network_dns { 'default':
-  ensure  => 'present',
-  servers => ['8.8.8.8', '8.8.4.4'],
-  search  => ['puppet.vm']
-}
+#network_dns { 'default':
+#  ensure  => 'present',
+#  servers => ['8.8.8.8', '8.8.4.4'],
+#  search  => ['puppet.vm']
+#}
 ios_config { 'nameserver1':
        command => 'ntp server 1.1.1.1',
        idempotent_regex => 'ntp server 1.1.1.1'
