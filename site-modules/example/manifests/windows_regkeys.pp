ad4be8a30cb519a5022ccbaa783c8ca7b1f06709
nelsong27 <nelson.gomez@puppet.com>
2023-09-26 13:35:55 -0400

    Update windows_regkeys

--- site-modules/example/manifests/windows_regkeys.pp
+++ site-modules/example/manifests/windows_regkeys.pp
@@ -8,7 +8,7 @@ class example::windows_regkeys {
 
   registry_value { 'HKEY_LOCAL_MACHINE\Software\Demonstration\value1':
     type => string,
-    data => 'this is a value from puppet demo',
+    data => 'this is a value from Nelson',
   }
 
   registry_value { 'HKEY_LOCAL_MACHINE\Software\Demonstration\value2':
